ed7bd7a14eda479a6ac004320398d5583ca3d98b
Frank Castro <frank.castro.g@tecsup.edu.pe>
2026-01-25 00:50:57 -0500

    Eliminado analiticas de vercel

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
+
 import { ThemeProvider } from '@/components/theme-provider'
 import { SWRProvider } from '@/components/providers/swr-provider'
 import './globals.css'
@@ -45,7 +45,7 @@ export default function RootLayout({
             {children}
           </SWRProvider>
         </ThemeProvider>
-        <Analytics />
+
       </body>
     </html>
   )
