e5b3ba11cfbb096372c83cb68d6e9e1c922c8295
Frank Castro <frank.castro.g@tecsup.edu.pe>
2026-01-26 11:47:26 -0500

    Migracion de rol y permisos

---
+++ backend/prisma/migrations/20260126164700_add_rbac_tables/migration.sql
@@ -0,0 +1,56 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `rol` on the `Usuario` table. All the data in the column will be lost.
+  - Added the required column `rolId` to the `Usuario` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Usuario" DROP COLUMN "rol",
+ADD COLUMN     "rolId" INTEGER NOT NULL;
+
+-- DropEnum
+DROP TYPE "Rol";
+
+-- CreateTable
+CREATE TABLE "Rol" (
+    "id" SERIAL NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "descripcion" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Rol_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Permiso" (
+    "id" SERIAL NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "descripcion" TEXT,
+
+    CONSTRAINT "Permiso_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "RolPermiso" (
+    "rolId" INTEGER NOT NULL,
+    "permisoId" INTEGER NOT NULL,
+
+    CONSTRAINT "RolPermiso_pkey" PRIMARY KEY ("rolId","permisoId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Rol_nombre_key" ON "Rol"("nombre");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Permiso_nombre_key" ON "Permiso"("nombre");
+
+-- AddForeignKey
+ALTER TABLE "Usuario" ADD CONSTRAINT "Usuario_rolId_fkey" FOREIGN KEY ("rolId") REFERENCES "Rol"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RolPermiso" ADD CONSTRAINT "RolPermiso_rolId_fkey" FOREIGN KEY ("rolId") REFERENCES "Rol"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RolPermiso" ADD CONSTRAINT "RolPermiso_permisoId_fkey" FOREIGN KEY ("permisoId") REFERENCES "Permiso"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
